d6993368289c57048aca72b9c416bfbb1c80b9a9
ljsolano17 <ljsolano17@hotmail.com>
2023-10-17 17:19:47 -0600

    Add API
    Without Mapping
    Returns Foci without groups object

---
+++ Solution/Solution.API/Controllers/FocusController.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Solution.DAL.EF;
+using data = Solution.DO.Objects;
+
+namespace Solution.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FociController : ControllerBase
+    {
+        private readonly SolutionDbContext _context;
+
+        public FociController(SolutionDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Foci
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<data.Focus>>> GetFocus()
+        {
+            return new Solution.BS.Focus(_context).GetAll().ToList();
+        }
+
+        // GET: api/Foci/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<data.Focus>> GetFocus(int id)
+        {
+            //var focus = await new Solution.BS.Focus(_context).GetOneByIdWithAsync(id);
+
+            var focus = new Solution.BS.Focus(_context).GetOneById(id);
+
+            if (focus == null)
+            {
+                return NotFound();
+            }
+
+            return focus;
+        }
+
+        // PUT: api/Foci/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutFocus(int id, data.Focus focus)
+        {
+            if (id != focus.FocusId)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                new Solution.BS.Focus(_context).Update(focus);
+            }
+            catch (Exception ee)
+            {
+                if (!FocusExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Foci
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<data.Focus>> PostFoci(data.Focus focus)
+        {
+
+            new Solution.BS.Focus(_context).Insert(focus);
+
+            return CreatedAtAction("GetFocus", new { id = focus.FocusId }, focus);
+        }
+
+        // DELETE: api/Foci/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<data.Focus>> DeleteFocus(int id)
+        {
+
+            var focus = new Solution.BS.Focus(_context).GetOneById(id);
+            if (focus == null)
+            {
+                return NotFound();
+            }
+
+            new Solution.BS.Focus(_context).Delete(focus);
+
+            return focus;
+        }
+
+        private bool FocusExists(int id)
+        {
+            return (new Solution.BS.Focus(_context).GetOneById(id) != null);
+        }
+    }
+}
---
+++ Solution/Solution.API/Controllers/GroupController.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Solution.DAL.EF;
+using data = Solution.DO.Objects;
+
+namespace Solution.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GroupsController : ControllerBase
+    {
+        private readonly SolutionDbContext _context;
+
+        public GroupsController(SolutionDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Groups
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<data.Group>>> GetGroups()
+        {
+            return new  Solution.BS.Group(_context).GetAll().ToList();
+        }
+
+        // GET: api/Groups/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<data.Group>> GetGroup(int id)
+        {
+            
+            var group =  new Solution.BS.Group(_context).GetOneById(id);
+
+
+            if (group == null)
+            {
+                return NotFound();
+            }
+
+            return group;
+        }
+
+        // PUT: api/Groups/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutGroup(int id, data.Group group)
+        {
+            if (id != group.GroupId)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                new Solution.BS.Group(_context).Update(group);
+            }
+            catch (Exception ee)
+            {
+                if (!GroupExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Groups
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<data.Group>> PostGroup(data.Group group)
+        {
+           
+            new Solution.BS.Group(_context).Insert(group);
+
+            return CreatedAtAction("GetGroup", new { id = group.GroupId }, group);
+        }
+
+        // DELETE: api/Groups/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<data.Group>> DeleteGroup(int id)
+        {
+            
+            var group = new Solution.BS.Group(_context).GetOneById(id);
+            if (group == null)
+            {
+                return NotFound();
+            }
+
+            new Solution.BS.Group(_context).Delete(group);
+
+            return group;
+        }
+
+        private bool GroupExists(int id)
+        {
+            //Validar si existe el grupo
+            return (new Solution.BS.Group(_context).GetOneById(id)!=null);
+        }
+    }
+}
